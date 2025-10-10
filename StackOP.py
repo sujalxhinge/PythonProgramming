3a80a8ea1c01e13cf07480903a713141e9c3c64a
sujal hinge <sujalhinge079@gmail.com>
2025-10-10 12:09:25 +0530

    10-0ctober-2025 Stack DSA

---
+++ StackOP.py
@@ -0,0 +1,9 @@
+stack = []
+stack.append(10)
+stack.append(20)
+stack.append(30)
+stack.append(40)
+print("stack Values", stack)
+stack.pop()
+print("After Pop ", stack)
+print("Top Element ", stack[-1])
